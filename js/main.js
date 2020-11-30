1f31d2a89cc330222014466726fb8c8ab7e04c66
Nikolai von Veh <nikolai.von.veh@obsidiandata.com.au>
2020-11-30 14:10:49 +1100

    Serviceability

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let calculate = () => {
+  let employmentIncome = $('#employmentIncome').val();
+  let additionalIncome = $('#additionalIncome').val();
+  let rentalIncome = $('#rentalIncome').val();
+
+  let monthlyExpenses = $('#monthlyExpenses').val();
+  let otherDebt = $('#otherDebt').val();
+  let homeLoan = $('#homeLoan').val();
+
+  let url = `https://06hi60fmrf.execute-api.ap-southeast-2.amazonaws.com/serviceability?employmentIncome=${employmentIncome}&additionalIncome=${additionalIncome}&rentalIncome=${rentalIncome}&monthlyExpenses=${monthlyExpenses}&otherDebt=${otherDebt}&homeLoan=${homeLoan}`;
+
+  $.ajax({
+    url: url,
+    crossDomain: true,
+    success: function(data) {
+      $('#remainingAmount').text('Remaining Amount: ' + data.remainingAmount);
+    }
+  });
+
+  return false;
+}
+
+$(function() {
+  $("#prospects_form").submit(function(e) {
+    e.preventDefault();
+  });
+
+  $('#calculate').click(calculate);
+});
